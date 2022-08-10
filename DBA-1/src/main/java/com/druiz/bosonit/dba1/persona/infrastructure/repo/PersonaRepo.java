f9555fc604a78fee19f1c2b5547e157acb00d37c
druiz912 <druiz264@gmail.com>
2022-08-10 11:15:52 +0200

    created dba-1 criteriaBuilder and fix other errors

--- DBA-1/src/main/java/com/druiz/bosonit/dba1/persona/infrastructure/repo/PersonaRepo.java
+++ DBA-1/src/main/java/com/druiz/bosonit/dba1/persona/infrastructure/repo/PersonaRepo.java
@@ -3,11 +3,10 @@ package com.druiz.bosonit.dba1.persona.infrastructure.repo;
 import com.druiz.bosonit.dba1.persona.domain.PersonaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
 import java.util.List;
 
 public interface PersonaRepo extends JpaRepository<PersonaEntity, Integer> {
 
-    public List<PersonaEntity> getData(HashMap<String, Object> conditions);
+    public List<PersonaEntity> findByName(String name);
 }
 
