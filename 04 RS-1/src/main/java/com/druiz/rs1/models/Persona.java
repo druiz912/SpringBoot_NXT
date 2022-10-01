3d12162b2c3580ddb137269354a225967b81318e
druiz912 <druiz264@gmail.com>
2022-10-01 20:22:35 +0200

    code cleanup and logic improvement for the business layer

--- 04 RS-1/src/main/java/com/druiz/rs1/models/Persona.java
+++ 04 RS-1/src/main/java/com/druiz/rs1/models/Persona.java
@@ -2,22 +2,61 @@ package com.druiz.rs1.models;
 
 
 import org.springframework.stereotype.Service;
-@Service
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Persona {
-        private int id;
+        private static AtomicInteger count = new AtomicInteger(0);
+        private Integer id;
         private String name;
+        private String city;
+        private Integer age;
+
+        public Persona(String name, String city, String age) {
+                this.name = (name.equals("")) ? null : name;
+                this.city = (city.equals("")) ? null : city;
+                this.age  = (age.equals(""))  ? null : Integer.parseInt(age);
+                this.id = count.incrementAndGet();
+        }
+
+        public Integer getId() {
+                return this.id;
+        }
+
+        public String getName() {
+                return this.name;
+        }
+
+        public String getCity() {
+                return this.city;
+        }
+
+        public Integer getAge() {
+                return this.age;
+        }
+
         public void setName(String name) {
                 this.name = name;
         }
-        public void setId(int id) {
-                this.id = id;
+
+        public void setCity(String city) {
+                this.city = city;
         }
-        public int getId(int id){
-                return id;
+
+        public void setAge(Integer age) {
+                this.age = age;
         }
-        public String getName() {
-                return name;
+
+        @Override
+        public String toString() {
+                return "Person{" +
+                        "id=" + id +
+                        ", name='" + name + '\'' +
+                        ", city='" + city + '\'' +
+                        ", age=" + age +
+                        '}';
         }
 }
+
 
 
