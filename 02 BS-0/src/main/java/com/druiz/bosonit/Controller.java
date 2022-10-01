5a5fb830f42fb3fae5f84ae19c574a496064f565
druiz912 <druiz264@gmail.com>
2022-10-01 19:36:50 +0200

    re-order project and clean code

--- 02 BS-0/src/main/java/com/druiz/bosonit/Controller.java
+++ 02 BS-0/src/main/java/com/druiz/bosonit/Controller.java
@@ -5,82 +5,23 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@RequestMapping(value = "person")
 public class Controller {
 
-    @GetMapping
-    String hello(){
-        return "Hello world! ";
+    @GetMapping("/{name}")
+    String hello(@PathVariable String name){
+        return "Hello " + name + "!";
     }
 
-    @GetMapping("users")
-    Persona getPersona(String name, String ciudad, int edad){
-        Persona per = new Persona();
-        per.setNombre("Daniel");
-        per.setCiudad("Jerez");
-        per.setEdad(22);
-        return per;
-    }
-
-    @GetMapping("users/{nombre}")
-    String getHello(@PathVariable String nombre){
-       return "Hello " + nombre;
-    }
-
-    @PostMapping("useradd")
-    Persona newPerson(@RequestBody ObjectNode requestObject) {
+    @PostMapping
+    public Persona createPerson(@RequestBody ObjectNode requestObject) {
         Persona newPerson =
                 new Persona(
                         requestObject.get("nombre").asText(),
                         requestObject.get("ciudad").asText(),
-                        requestObject.get("edad").asInt());
-        newPerson.setSumEd();
+                        requestObject.get("edad").asText());
+        newPerson.addOneAge();
         return newPerson;
-
-    }
-
-    class Persona {
-
-        private String nombre;
-        private String ciudad;
-        private int edad;
-
-        public Persona(String nombre, String ciudad, int edad) {
-            this.nombre = nombre;
-            this.ciudad = ciudad;
-            this.edad = edad;
-        }
-
-        public Persona() {
-
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public String getCiudad() {
-            return ciudad;
-        }
-
-        public void setCiudad(String ciudad) {
-            this.ciudad = ciudad;
-        }
-
-        public int getEdad() {
-            return edad;
-        }
-
-        public void setEdad(int edad) {
-            this.edad = edad;
-        }
-
-        public int setSumEd(){
-            return getEdad() + 1;
-        }
     }
 }
 
