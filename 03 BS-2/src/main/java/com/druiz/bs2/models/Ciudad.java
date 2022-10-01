e1f2681ae0edfd486dd9a9eb6467e4d217c3bd26
druiz912 <druiz264@gmail.com>
2022-10-01 20:00:59 +0200

    use lombok annotations

--- 03 BS-2/src/main/java/com/druiz/bs2/models/Ciudad.java
+++ 03 BS-2/src/main/java/com/druiz/bs2/models/Ciudad.java
@@ -1,33 +1,14 @@
 package com.druiz.bs2.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ciudad {
-
     private String nombre;
     private int numeroHabitantes;
 
-    public Ciudad(String nombre, int numeroHabitantes) {
-        this.nombre = nombre;
-        this.numeroHabitantes = numeroHabitantes;
-    }
-
-    public Ciudad(){}
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getNumeroHabitantes() {
-        return numeroHabitantes;
-    }
-
-    public void setNumeroHabitantes(int numeroHabitantes) {
-        this.numeroHabitantes = numeroHabitantes;
-    }
 }
